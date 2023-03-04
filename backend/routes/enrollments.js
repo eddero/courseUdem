5349dba2994742d9b5f4ae885c623cf67c49257b
eddero <edvin97johansson@hotmail.com>
2023-03-04 22:42:05 +0100

    add a couple of stuff

--- backend/routes/enrollments.js
+++ backend/routes/enrollments.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const {} = require("../controllers/enrollmentController")
+
 const router = express.Router();
 
+router.get("/");
+
+router.get("/:id");
+
+router.post("/");
+
+router.put("/");
+
+router.delete("/delete/:id");
 
 module.exports = router;
